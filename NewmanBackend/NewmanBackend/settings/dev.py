2d97d56c438e5952dd33d49ee57603c6607d0df6
H1ghw4y <3316956690@qq.com>
2022-12-08 22:05:41 +0800

    .

--- NewmanBackend/NewmanBackend/settings/dev.py
+++ NewmanBackend/NewmanBackend/settings/dev.py
@@ -155,7 +155,7 @@ CACHES = {
     },
     "session": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1：3306/1", #换成自己的
+        "LOCATION": "redis://127.0.0.1:3306/1", #换成自己的
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
